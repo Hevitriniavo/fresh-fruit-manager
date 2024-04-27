11e55898cd1de245e624d09ccdb2345bf365892d
Hevitriniavo <hei.tantely@gmail.com>
2024-04-27 10:03:25 +0545

    chore: refactoring class Product

--- src/main/java/com/tantely/freshfruitmanager/repositories/ProductRepository.java
+++ src/main/java/com/tantely/freshfruitmanager/repositories/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository  extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByNameIgnoreCaseContainingAndPriceBetween(String searchName, Double priceMin, Double priceMax, Pageable pageable);
 
     Page<Product> findAllByPriceBetween(Double priceMin, Double priceMax, PageRequest pageRequest);
